fd774be3348832faec28ed8664095c9a1164e4a9
madhu <madhumapril@gmail.com>
2026-07-10 11:44:17 +0530

    updated some changes

--- script.js
+++ script.js
@@ -186,3 +186,9 @@ var typed = new Typed(".typing", {
     loop: true
 
 });
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove", (e) => {
+    glow.style.left = e.clientX + "px";
+    glow.style.top = e.clientY + "px";
+});
